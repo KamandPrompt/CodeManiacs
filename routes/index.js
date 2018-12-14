266b4a5068b1dddad0b41129a78c895e27c2b196
ViPuL <vsvipul555@gmail.com>
2018-12-14 22:28:32 +0530

    add email password authentication api (#6)
    * add email password authentication api
    * add gitignore

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,47 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var userService = require('../authenticate');
 
 router.get('/', (req, res, next) => {
     res.send('Welcome CodeManiacs');
 });
 
+router.post('/signup', function(req, res){
+    var email = req.headers['email'];
+    var password = req.headers['password'];
+    userService.signUp(email, password,
+        function(){
+            res.status(201).send('User signed up');
+        },function(err){
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
+});
+
+router.post('/signin', function(req, res){
+    var email = req.headers['email'];
+    var password = req.headers['password'];
+    userService.signIn(email, password,
+        function(){
+            res.status(200).send('User signed in');
+        },function(err){
+        if (err) {
+            res.status(401).send(err);
+        }
+    });
+});
+
+router.post('/signout', function(req, res){
+    userService.signOut(
+        function(){
+            res.status(200).send('User signed out');
+        },function(err){
+            if (err) {
+                res.status(401).send(err);
+            }
+        });
+});
 
 module.exports = router;
