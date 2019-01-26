55027b0e877bd5fb5f305cdc9b6b2060fe6773f5
dipanshu <43211840+dipanshu231099@users.noreply.github.com>
2019-01-26 19:56:40 +0530

    Problems and Testcases schemas added
    * my first commit for a general message
    
    * my first commit for a general message
    
    * change of plans folks
    
    * change of plans folks
    
    * update_home_page
    
    * name change of folder
    
    * home_page
    
    * homepage update
    
    * iss30
    
    * syntax_corrections
    
    * modify model
    
    * reedited_how they came in?_changes
    
    * intrgr to number
    
    * Closes #30

---
+++ models/problems.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+var prob = new mongoose.Schema ({
+    qID: Number,
+    name: String,
+    description: String,
+    inputFormat: String,
+    outputFormat: String,
+    constraints: String,
+    sampleInput: String,
+    sampleOutput: String,
+    explanation: String,
+    difficulty: Number,
+    problemSetter: String,
+    timeLimit: Number,
+    memoryLimit: Number,
+    tags: String,
+    editorial: String,
+});
+var Problems = mongoose.model('Problems',prob);
+module.exports = Problems;
