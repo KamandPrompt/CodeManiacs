f5e280af43eb139e0ab2a63b9b2382aa8f21b47c
dipanshu231099 <dipanshu231099@gmail.com>
2019-01-26 18:30:28 +0530

    modify model

--- models/problems.js
+++ models/problems.js
@@ -1,49 +1,20 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/myappdatabase');
-var prob = new mongoose.Schema({
-    
+var prob = new mongoose.Schema ({
     qID: Integer,
-
     name: String,
-
     description: String,
-
     inputFormat: String,
-
     outputFormat: String,
-
     constraints: String,
-
     sampleInput: String,
-
     sampleOutput: String,
-
     explanation: String,
-
     difficulty: Integer,
-
     problemSetter: String,
-
     timeLimit: Integer,
-
     memoryLimit: Integer,
-
     tags: String,
-
     editorial: String,
 });
-
 var Problems = mongoose.model('Problems',prob);
 module.exports = Problems;
-
-
-
-
-
-
-
-
-
-
-
-
--- models/testcases.js
+++ models/testcases.js
@@ -1,18 +1,9 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/myappdatabase');
-
 var test = new mongoose.Schema ( {
-    
     qID: Integer,
-
     timeLimit: Integer,
-
     memoryLimit: Integer,
-
     cases: [{stdin: String, stdout: String}]
-
 });
-
 var Testcases = mongoose.model('Testcases',test);
-
 module.exports = Testcases;
