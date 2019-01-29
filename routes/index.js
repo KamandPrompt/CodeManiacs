7a44eafd96a99e17d042804db564b99e59bc1dc9
vsvipul <vsvipul555@gmail.com>
2019-01-29 17:44:46 +0530

    fix ejs passing user

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("index");
+    res.render("index", {user: req.user});
 });
 
 router.get("/problems/all", (req, res) => {
