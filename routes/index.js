44fdaa1e4082adcd244b7fed2786cc4c0c25bfa0
dipanshu231099 <dipanshu231099@gmail.com>
2019-02-01 12:13:37 +0530

    done

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ router.get("/", (req, res) => {
     res.render("index");
 });
 
-router.get("/problems/all", (req, res) => {
-    res.render("problem_set");
+router.get("/problems/all", (req, res) => {    
     const ProblemsModel = require("../models/problems");
-    ProblemsModel.find({isVisible: 1},{id: -1,name: 1,tags: 1,difficulty: 1}).sort({id: -1}).then(doc => console.log(doc));
+    var table = ProblemsModel.find({isVisible: 1},{id: -1,name: 1,tags: 1,difficulty: 1}).sort({id: -1}).then(function(doc){
+        res.render("problem_set",{db: doc});
+    });
 });
 
 router.get("/problem/:qID", (req, res) => {
