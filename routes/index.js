c189a4b9a2772417a0d92d45e7a284270c12c320
Vishal1541 <vishal.nnd0@gmail.com>
2019-02-11 15:19:27 +0530

    added contribution page, modified homepage!

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ router.get("/", (req, res) => {
     res.render("index");
 });
 
+router.get("/contribution", (req, res) => {
+    res.render("contribution");
+});
+
 router.get("/contests", contests.showContests);
 
 router.get("/problems/all", problems.problemSet);
