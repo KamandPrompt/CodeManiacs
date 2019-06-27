66a8ebfbe6460a8f178eb8f81a4a70e6064d2076
karry26 <47284842+karry26@users.noreply.github.com>
2019-06-27 22:54:05 +0530

    issue 114

--- public/js/solution_submit.js
+++ public/js/solution_submit.js
@@ -64,3 +64,27 @@ const submitSolution = () => {
 const goBack = () => {
 	window.location.href = "/problem/" + question_id ;
 }
+const copyclipboard=( )=>{
+   
+  const el = document.createElement('textarea');
+  el.value = editor.getValue();
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+    
+  document.body.removeChild(el);
+}
+
+function download() {
+  var text = editor.getValue();
+    text = text.replace(/\n/g, "\r\n"); // To retain the Line breaks.
+    var blob = new Blob([text], { type: "text/plain"});
+    var anchor = document.createElement("a");
+    anchor.download = "code.txt";
+    anchor.href = window.URL.createObjectURL(blob);
+    anchor.target ="_blank";
+    anchor.style.display = "none"; // just to be safe!
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+}
